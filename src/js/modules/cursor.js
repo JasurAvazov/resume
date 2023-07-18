38c5e504b62d16899d9137fd0fe7e9c9b6efcae7
JasurAvazov <jasuravazov4@gmail.com>
2023-07-18 21:30:56 +0500

    about

--- src/js/modules/cursor.js
+++ src/js/modules/cursor.js
@@ -1,8 +1,38 @@
 export function init() {
+	// var cursor = document.querySelector(".custom-cursor");
+	// var secondCursor = document.querySelector(".custom-second-cursor");
+
+	// document.addEventListener("mousemove", function (event) {
+	// 	cursor.style.left = event.clientX + "px";
+	// 	cursor.style.top = event.clientY + "px";
+	// 	secondCursor.style.left = event.clientX + "px";
+	// 	secondCursor.style.top = event.clientY + "px";
+	// });
 	var cursor = document.querySelector(".custom-cursor");
+	var secondCursor = document.querySelector(".custom-second-cursor");
+
+	var lag = 100; // Lag duration in milliseconds
+	var mouseX = 0;
+	var mouseY = 0;
+	var lagX = 0;
+	var lagY = 0;
 
 	document.addEventListener("mousemove", function (event) {
-		cursor.style.left = event.clientX + "px";
-		cursor.style.top = event.clientY + "px";
+		mouseX = event.clientX;
+		mouseY = event.clientY;
 	});
+
+	function updateCursor() {
+		lagX += (mouseX - lagX) / 10;
+		lagY += (mouseY - lagY) / 10;
+
+		cursor.style.left = mouseX + "px";
+		cursor.style.top = mouseY + "px";
+		secondCursor.style.left = lagX + "px";
+		secondCursor.style.top = lagY + "px";
+
+		requestAnimationFrame(updateCursor);
+	}
+
+	updateCursor();
 }
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -2,5 +2,9 @@ import Swiper from 'swiper';
 
 export function init() {
 	const worksSwiper = new Swiper('.works-swiper', {
+        pagination: {
+            el: '.works-swiper-pagination',
+            clickable: true,
+        },
     });
 }
